7654d00e43663fdfb25501b2703397597a7903c6
johnwoodill <johnwoodill@gmail.com>
2017-12-07 16:41:59 -1000

    updating cbb model for paper

--- opt_threshold.R
+++ opt_threshold.R
@@ -33,29 +33,49 @@ cherryonfarm <- cherryonfarm[3:12]
 # gridd <- filter(gridd, Var4 >= 0 & Var3 > 0)
 # head(gridd)
 
+# infdat <- structure(list(abl = c(0.055, 0.04640625, 0.0378125, 0.0346614583333333, 
+# 0.0315104166666667, 0.028359375, 0.0220572916666667, 0.0157552083333333, 
+# 0.009453125, 0.00630208333333333), abd = c(0.025, 0.0034375, 
+# 0.00171875, 0.00372395833333333, 0.00501302083333333, 0.00315104166666667, 
+# 0.00315104166666667, 0.00315104166666667, 0.00630208333333333, 
+# 0.00630208333333333), cd = c(0.01, 0.0384895833333333, 0.0274189814814815, 
+# 0.0285648148148148, 0.0304267939814815, 0.0354398148148148, 0.0366790674603175, 
+# 0.0383962673611111, 0.0415473090277778, 0.0557889093137255), 
+#     nin = c(0.91, 0.911666666666667, 0.933049768518519, 0.933049768518519, 
+#     0.933049768518519, 0.933049768518519, 0.938112599206349, 
+#     0.942697482638889, 0.942697482638889, 0.931606924019608)), .Names = c("abl", 
+# "abd", "cd", "nin"), row.names = c(NA, -10L), class = "data.frame")
+
+source("3-dynamic_economic_model.R")
+
+infdat <- data.frame(abl = totalnb$field_ablive,
+                     abd = totalnb$field_abdead,
+                     cd = totalnb$field_cd)
 
 thold_search <- function(x){
-      threshold <- c(x, 0, 0)
+      threshold <- c(x[1], infdat[i, 2], infdat[i, 3])
       threshold[4] <- 1 - sum(threshold[1:3])
       nspray <- threshold %*% nsp_mcListFit$estimate[[i]][]
       nspray_growth <- nspray[3] - threshold[3]
+      
       nsp_damage <- nspray_growth * (cherryonfarm[i]*threshold[4]) * cherrypricing(nspray[3])
       
       # Get decision : 1 (spray), 0 (no spray)
-      ifelse(nsp_damage >= cost_s*acres, ifelse(sum(threshold[1:3]) <= 1, nsp_damage - cost_s*acres, 99999), 99999)
-      # ret <- nsp_damage - (cost_s*acres)
-      # ifelse(ret >= 0, ret, 9999)
+      ifelse(nsp_damage >= cost_s*acres, 
+             ifelse(sum(threshold[1:3]) <= 1, nsp_damage - cost_s*acres, 99999), 99999)
+      #nsp_damage - (cost_s*acres)
+      #ifelse(ret >= 0, ret, 9999)
 }
 
 dat <- data.frame()
 for (i in 1:9){
-  optvalue <- optim(c(0.00001), thold_search,
+  optvalue <- optim(c(infdat[i, 1]), thold_search,
       lower = 0, upper = 1, method = "L-BFGS-B")
   optvalue
-  thold <- data.frame(month = i + 3,
+  thold <- data.frame(month = i + 2,
                       ABLive = optvalue$par[1],
-                      ABDead = 0,
-                      CD = 0)
+                      ABDead =  infdat[i, 2],
+                      CD = infdat[i, 3])
   dat <- rbind(dat, thold)
 }
 dat
--- usda_calibration.R
+++ usda_calibration.R
@@ -8,7 +8,7 @@ library(cowplot)
 setwd("/run/media/john/1TB/SpiderOak/Projects/cbb-dynamic-model/")
 # usda <- read_csv("data/KonaS1_unmanaged.csv")
 usda <- read_csv("data/USDA_Kona_damage_S1.csv")
-# usda <- read_csv("data/Kona_Damage_S2_2017.csv")
+#usda <- read_csv("data/Kona_Damage_S2_2017.csv")
 #usda$date <- match(usda$date,month.abb)
 #spray <- read_csv("data/S1_management.csv")
 
@@ -20,9 +20,9 @@ usda$month <- month(usda$date)
 #usda[is.na(usda$AB_)] <- 1
 #usda[is.na(usda)] <- 0
 usda
-# Subset farms that sprayed throughout the year
+## Subset farms that sprayed throughout the year
 usda <- filter(usda, site == "Kelleher" | site == "O'Neill")
-#usda <- filter(usda, site == "Kelleher Farm" | site == "O'Neill Farm")
+# usda <- filter(usda, site == "Kelleher Farm" | site == "O'Neill Farm")
 #sda <- filter(usda, site == "Kelleher")
 #usda <- filter(usda, site == "O'Neill")
 usda <- filter(usda, total_dissected != 0)
@@ -79,36 +79,39 @@ newdat <- usda %>%
 
 newdat[is.na(newdat)] <- 0
 
-minmax <- usda %>% 
-  group_by(month)  %>% 
-  summarise(percent_ab_live_high = max(percent_ab_live, na.rm = TRUE),
-            percent_ab_live_low = min(percent_ab_live, na.rm = TRUE),
-            percent_ab_dead_high = max(percent_ab_dead, na.rm = TRUE),
-            percent_ab_dead_low = min(percent_ab_dead, na.rm = TRUE),
-            percent_cd_live_high = max(percent_cd_live, na.rm = TRUE),
-            percent_cd_live_low = min(percent_cd_live, na.rm = TRUE),
-            percent_absent_high = max(percent_absent, na.rm = TRUE),
-            percent_absent_low = min(percent_absent, na.rm = TRUE))
+# minmax <- usda %>% 
+#   group_by(month)  %>% 
+#   summarise(percent_ab_live_high = max(percent_ab_live, na.rm = TRUE),
+#             percent_ab_live_low = min(percent_ab_live, na.rm = TRUE),
+#             percent_ab_dead_high = max(percent_ab_dead, na.rm = TRUE),
+#             percent_ab_dead_low = min(percent_ab_dead, na.rm = TRUE),
+#             percent_cd_live_high = max(percent_cd_live, na.rm = TRUE),
+#             percent_cd_live_low = min(percent_cd_live, na.rm = TRUE),
+#             percent_absent_high = max(percent_absent, na.rm = TRUE),
+#             percent_absent_low = min(percent_absent, na.rm = TRUE))
 
-minmax[is.na(minmax)] <- 0
+# minmax[is.na(minmax)] <- 0
 #newdat$ni <- 100 - (newdat$percent_ab_dead + newdat$percent_ab_live + newdat$percent_cd_live)
 
-newdat <- left_join(newdat, minmax, by = "month")
+#newdat <- left_join(newdat, minmax, by = "month")
 
 g <- gather(newdat, key = position, value, -month)
 g
 ggplot(g, aes(month, value, color = position)) +geom_line() + ylab("Percent Infestation (Field Level)") + xlab("Month") + scale_x_continuous(breaks = 3:12)
 
-p1 <- ggplot(newdat, aes(month, percent_ab_live)) + geom_line()+ ggtitle("AB LIVE") + scale_x_continuous(breaks = 3:12) + geom_line(aes(month, percent_ab_live_high), linetype = "dotted")+ geom_line(aes(month, percent_ab_live_low), linetype = "dotted")
+p1 <- ggplot(newdat, aes(month, percent_ab_live)) + geom_line()+ ggtitle("AB LIVE") + scale_x_continuous(breaks = 3:12) 
+# +  geom_line(aes(month, percent_ab_live_high), linetype = "dotted")+ geom_line(aes(month, percent_ab_live_low), linetype = "dotted")
 p1
-p2 <- ggplot(newdat, aes(month, percent_ab_dead)) + geom_line() + ggtitle("AB DEAD")+ scale_x_continuous(breaks = 3:12) + geom_line(aes(month, percent_ab_dead_high), linetype = "dotted")+ geom_line(aes(month, percent_ab_dead_low), linetype = "dotted")
+p2 <- ggplot(newdat, aes(month, percent_ab_dead)) + geom_line() + ggtitle("AB DEAD")+ scale_x_continuous(breaks = 3:12) 
+#+ geom_line(aes(month, percent_ab_dead_high), linetype = "dotted")+ geom_line(aes(month, percent_ab_dead_low), linetype = "dotted")
 p2
-p3 <- ggplot(newdat, aes(month, percent_cd_live)) + geom_line() + ggtitle("CD")+ scale_x_continuous(breaks = 3:12)  + geom_line(aes(month, percent_cd_live_high), linetype = "dotted")+ geom_line(aes(month, percent_cd_live_low), linetype = "dotted")
+p3 <- ggplot(newdat, aes(month, percent_cd_live)) + geom_line() + ggtitle("CD")+ scale_x_continuous(breaks = 3:12)  
+#+ geom_line(aes(month, percent_cd_live_high), linetype = "dotted")+ geom_line(aes(month, percent_cd_live_low), linetype = "dotted")
 p3
 plot_grid(p1,p2,p3, ncol = 2)
 
-
+newdat
 
 # Subset farms that sprayed throughout the year
-dat <- filter(usda, site == "Kelleher" | site == "O'Neill")
-ggplot(dat, aes(month, CD_alive, color = site)) + geom_point()
+# dat <- filter(usda, site == "Kelleher" | site == "O'Neill")
+# ggplot(dat, aes(month, CD_alive, color = site)) + geom_point()
